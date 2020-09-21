fbed689c12dc727ca3d12fc811339c030eeb110f
Hasan F Mohammad <hfmoha01@louisville.edu>
2020-09-21 17:03:44 -0400

    Add Calendar view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace CIS_420_Project.Controllers
             return View();
         }
 
+        public IActionResult Calendar()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
