b75ef2e2a18db1ecc38fb9fe4528ae5231540e7b
Aaron M White <amwhit05@louisville.edu>
2020-10-14 16:57:16 -0400

    Created Industry Area

---
+++ Areas/Industry/Controllers/FundingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIS_420_Project.Areas.Industry.Controllers
+{
+    public class FundingController : Controller
+    {
+        [Area("Industry")]
+        public IActionResult Main()
+        {
+            return View();
+        }
+    }
+}
