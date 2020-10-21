405f36b8c738585e293fd1376ca675f2d3532267
tlyon1022 <54088092+tlyon1022@users.noreply.github.com>
2020-10-20 20:56:56 -0400

    compliance page

---
+++ Areas/Researchers/Controllers/ComlianceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIS_420_Project.Areas.Researchers.Controllers
+{
+    public class ComplianceController : Controller
+    {
+        [Area("Researchers")]
+        public IActionResult Main()
+        {
+            return View();
+        }
+    }
+}
